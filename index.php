15c3cf3e5dacfff41d9bc69acbc7019c9306698e
bryce-guglietti <98065505+bryce-guglietti@users.noreply.github.com>
2022-07-13 20:46:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
                         echo "<h2>Hello, " . $_SESSION["useruid"] . "!</h2>";
                         $userid = $_SESSION["usersid"];
                         countAlbums($conn, $userid);
+                        echo "<h3>Search through your collection, add to it, or remove from it</h3>";
                     }
                     else{
                         echo "<p>This Website uses a fully functional login system to keep an inventory of 
